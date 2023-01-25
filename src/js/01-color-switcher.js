af4cbc343acb9fd7ea93c0cbabbcc9620b06e153
vikiv5 <vi_kov@ukr.net>
2023-01-26 01:10:51 +0200

    color switch

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector ("body") ;
 
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]") ;
+
+let timerId = null ;
+
+startBtn.addEventListener ("click", onStartBtnClick) ;
+stopBtn.addEventListener ("click", onStopBtnClick); 
+
+function onStartBtnClick() {
+    timerId = setInterval(getRandomHexColor, 1000);
+    startBtn.setAttribute("disabled", true);
+    clearInterval(timerId);
+}
+
+function onStopBtnClick(){
+    startBtn.removeAttribute ("disabled")
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
